38ef00c71f84c01fcba794fa1a2709b972c64c1b
Danielly Costa <daniellycosta97@gmail.com>
2017-09-18 09:05:53 -0300

    Implementação das funções ON e OFF

--- src/index.ino
+++ src/index.ino
@@ -1,37 +1,82 @@
-#include<ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
 #include <ESP8266HTTPClient.h>
-#include <DNSServer.h>
-#include <Time.h>
-#include <Hash.h>
-#include <SocketIoClient.h>
+#include <WiFiUdp.h>
+
+#include <SocketIOClient.h>
+#include <ArduinoJson.h>
 #include <WiFiManager.h>
-#include<ArduinoJson.h>
 #include <DeviceData.h>
-//===================================================================================
-// DEFINES
-//===================================================================================
-#define HOST "10.7.227.77" // host de envio
-#define PORT 3000 // porta para envio
-#define GETDATAHORA "http://api.saiot.ect.ufrn.br/api/log/data-hora"
 
-//===================================================================================
+//DEFINES
+#define HOST "192.168.0.108" // host de envio
+#define PORT 3001 // porta para envio
+#define GETDATAHORA "http://api.saiot.ect.ufrn.br/v1/history/data-hora"
+
+String getHoraAtual(void);
+String setJson(DeviceData _device);
+void sendStatus(String status);
+void turnOn(String status);
+void turnOff(String status);
+void configuration(String status);
+
 // VARIABLES
-//===================================================================================
-
-SocketIoClient socket;
+SocketIOClient socket;
 WiFiManager wifi;
 DeviceData device;
 
+extern String RID;
+extern String Rname;
+extern String Rcontent;
+
 String ipStr;
 String dataAtual;
-char devStatus[400];
 
-StaticJsonBuffer<400> jsonBuffer;
+void setup() {
+    Serial.begin(115200);
+    Serial.flush();
 
-//===================================================================================
-// FUNCTIONS
-//===================================================================================
+    wifi.autoConnect();
+
+    if (!socket.connect(HOST, PORT)) {
+      Serial.println("connection failed");
+      return;
+    } else if (socket.connected()) {
+      Serial.println("\n\n\n\n\nConectou no servidor\n\n\n");
+    }
+
+    IPAddress ip = WiFi.localIP();
+    ipStr = String(ip[0]) + "." + String(ip[1]) + "." + String(ip[2]) + "." + String(ip[3]);
+
+    dataAtual = getHoraAtual();
+
+    //EXEMPLO
+    device.setId("ESP001");
+    device.setTag("LAMPADA_LAB");
+    device.setDescription("Lampada que se encontra no lab 402(LII)");
+    device.setDeviceIp(ipStr);
+    device.setDateTime(dataAtual);
+    device.setActionAdress("rota/postar/dados");
+    device.setCommType(1); //1 - síncrona, 0 - assíncrona
+    device.setState(1); //1 - ON, 0- OFF
+    device.setTimeout(5);
+    device.setIntensity(50.5);
+    device.setConsumption(500.255);
+    device.setDeadBand(0.3);
+
+    socket.emit("valu", "OOOII");
+    socket.on("sla", sendStatus);
+    socket.on("on", turnOn);
+    socket.on("off", turnOff);
+    //socket.on("configuration", configuration);
+}
+
+void loop() {
+    socket.monitor();
+}
+
+//FUNCTIONS
 String getHoraAtual() {
   HTTPClient http;
   http.begin(GETDATAHORA);
@@ -44,70 +89,50 @@ String getHoraAtual() {
   return payload;
 }
 
-void setJson(DeviceData d){
+String setJson(DeviceData _device){
 
+  String devStatus;
+  //char devStatus[400];
+  StaticJsonBuffer<400> jsonBuffer;
   JsonObject& clientData = jsonBuffer.createObject();
 
-  clientData["deviceId"] = device.getId();
-  clientData["ip"] = device.getDeviceIp();
-  clientData["tag"] = device.getTag();
-  clientData["communicationType"] =device.getCommType();
-  clientData["description"] = device.getDescription();
-  clientData["timeout"] = device.getTimeout();
-  clientData["state"] = device.getState();
-  clientData["intensity"] = device.getIntensity();
-  clientData["consumption"] = device.getConsumption();
-  clientData["dateTime"] = device.getDateTime();
-  clientData["actionAdress"] = device.getActionAdress();
+  clientData["deviceId"] = _device.getId();
+  clientData["ip"] = _device.getDeviceIp();
+  clientData["tag"] = _device.getTag();
+  clientData["communicationType"] = _device.getCommType();
+  clientData["description"] = _device.getDescription();
+  clientData["timeout"] = _device.getTimeout();
+  clientData["state"] = _device.getState();
+  clientData["intensity"] = _device.getIntensity();
+  clientData["consumption"] = _device.getConsumption();
+  clientData["dateTime"] = _device.getDateTime();
+  clientData["actionAdress"] = _device.getActionAdress();
 
   clientData.printTo(devStatus);
+  Serial.println(devStatus);
+  return devStatus;
 }
 
-void sendStatus(const char * payload, size_t length){
-   setJson(device);
-  socket.emit("on_status", devStatus);
+void sendStatus(String state){
+  Serial.println(state);
+  Serial.println();
+  socket.emit("on_status", setJson(device));
+  Serial.println("Enviou JSON!");
 }
-
-//===================================================================================
-// SETUP
-//=================================================================================
-
-void setup(void) {
-
-  Serial.begin(115200); // inicia o serial
-  wifi.autoConnect(); // conectar no ultimo wifi salvo
-  socket.begin(HOST, PORT); // inicia a conexão com o servidor
-
-  IPAddress ip = WiFi.localIP();
-  ipStr = String(ip[0]) + "." + String(ip[1]) + "." + String(ip[2]) + "." + String(ip[3]);
-  
-  dataAtual = getHoraAtual();
-
-//EXEMPLO
-  device.setId("ESP001");
-  device.setTag("LAMPADA_LAB");
-  device.setDescription("Lampada que se encontra no lab 402(LII)");
-  device.setDeviceIp(ipStr);
-  device.setDateTime(dataAtual);
-  device.setActionAdress("rota/postar/dados");
-  device.setCommType(1); //1 - síncrona, 0 - assíncrona
-  device.setState(1); //1 - ON, 0- OFF
-  device.setTimeout(5);
-  device.setIntensity(50.5);
-  device.setConsumption(500.255);
-  device.setDeadBand(0.3);
-
-  socket.on("status", sendStatus);
-  //socket.on("on",);
-  //socket.on("off");
-  //socket.on("configuration")
-
+void turnOn(String status){
+  if(device.getState() == "Off"){
+  device.setState(1);
+  socket.emit("on_status", setJson(device));
+  Serial.println("Ligou!");
+  }
 }
-
-//===================================================================================
-// LOOP
-//===================================================================================
-
-void loop() {
-  socket.loop();
+void turnOff(String status){
+  if(device.getState() =="On"){
+  device.setState(0);
+  socket.emit("on_status", setJson(device));
+  Serial.println("Desligou!");
+  }
 }
+/*void configuration(String status){
+
+}*/
