230e8205464eadd3ed016b35c6912ffec69d8917
Danielly Costa <daniellycosta97@gmail.com>
2017-11-14 14:08:20 -0300

    Alterando nomes das classes

---
+++ lib/deviceData/src/IntensityDevice.h
@@ -0,0 +1,14 @@
+#ifndef IntensityDevice_h
+#define IntensityDevice_h
+
+#include <Arduino.h>
+#include "InstantData.h"
+
+class IntensityDevice: public InstantData{
+private:
+double consumption;
+
+public:
+};
+
+#endif
